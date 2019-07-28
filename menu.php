b10cfe34fa9cd45646a07f0fd65368c9090dc8da
sly92 <codingsly@gmail.com>
2019-07-28 23:47:38 +0200

    dreamTeam v1

---
+++ menu.php
@@ -0,0 +1,88 @@
+<?php
+<header id="entete"> 
+      <div id="contenu-entete">
+	      <div id="entete-haut">
+	  
+      <a href="accueil.html" title="Accueil"  >
+		<img src="photo_accueil/Dreamteam.png" alt="logo" width="130" height="120" style= "float:left; margin-top:7px;margin-left:10px;"/>
+		</a>
+			
+		<p1><strong> Saint Denis<br/> 
+      Dream Team </strong></p1>
+          
+        <div>
+			<a href="" target="_blank" title="Facebook">
+		<img class='FBTW' width="30" height="30" alt="logo" src="photo_accueil/facebook.png"></a>
+          
+			<a href="" target="_blank" title="Twitter">
+		<img class='FBTW' width="30" height="30" alt="logo" src="photo_accueil/twitter.png"></a>
+      
+      <a href=""><img id="SD" width="230" height="50" alt="logo" src="photo_accueil/Logo_CG_93.png"></a>
+       <a href=""><img id="SD" width="100" height="50" alt="logo" src="photo_accueil/logoFFBB.jpg"></a>
+        </div>
+          
+       </div>
+
+	<div id="entete-bas">
+			
+		<nav> 
+		<ul id="menu">
+			<li><a href = "accueil.html" class="active"> Accueil </a></li>
+			<li>
+				<a>Club</a>
+				<ul style="display:block">
+
+						<li><a href="basket_histoire.html">Histoire</a></li>
+						<li><a href="horaire.html">Planning</a></li>
+						
+				</ul>
+		   </li>
+			
+		  <li><a href = "#">Club</a>
+		  <ul style="display:block">
+
+						<li><a href="composition.html">Composition</a></li>
+						<li><a href="stats.html">Statistique</a></li>
+						<li><a href="classement.html">Classement</a></li>
+						
+				</ul>
+		   </li>
+
+
+		  <li><a href = "calendrier.html"> Calendrier</a>
+		  <ul style="display:block">
+						
+				</ul>
+		   </li>
+      
+       <li><a href = "forum.html"> Forum </a>
+          <ul style="display:block">
+						
+				</ul>
+		   </li>
+      
+		   <li><a href = "contact.html"> Contact </a>
+		   <ul style="display:block">
+			
+				</ul>
+      </li>
+      
+       <li><a href = "boutique.html" style="color:#d2aB70"> Boutique </a>
+  	   <ul style="display:block">
+						
+				</ul>
+		   </li>
+      
+		<li><a style="color:#d2aB70;" href="billetterie.html"> Billetterie </a>
+			<ul style="display:block">
+					 
+				</ul>
+		   </li>   
+		
+	 	</ul>
+		</nav>	
+          </div>
+        </div>
+	    </div>
+	</header>
+	?>
